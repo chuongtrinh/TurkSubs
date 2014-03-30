27657c36ef669c88488ce442c58862e4dffcbd10
subzeroice22 <laramie17goode@gmail.com>
2014-03-30 12:13:36 -0500

    merged Chuong's code with mine

--- TurkSubs/mainwindow.py
+++ TurkSubs/mainwindow.py
@@ -21,6 +21,7 @@ class MainWindow ( QMainWindow ):
         self.ui.setupUi( self )
         self.connect(self.ui.submitButton, SIGNAL('clicked()'),self.splitFile)
         self.connect(self.ui.resultButton, SIGNAL('clicked()'),self.get_hit_result)
+        self.connect(self.ui.deleteHitButton, SIGNAL('clicked()'),self.delete_all_hits)
 
     def __del__ ( self ):
         self.ui = None
@@ -103,26 +104,56 @@ class MainWindow ( QMainWindow ):
         #print self.hit_time_frame[my_hit_id]
         print my_hit_id
         #mtc.disable_hit(my_hit_id)
+    def startHit(self,Url,index):
+        #--------------- CREATE THE HIT -------------------
+        mtc = MTurkConnection(aws_access_key_id=ACCESS_ID, aws_secret_access_key=SECRET_KEY, host=HOST)
+        title = 'Write subtitles for a short movie clip ' + str(index)
+        description_notag = ('Watch a short clip and write down the dialogue between characters.' 
+                'If there is no dialogue, but there are prominent sounds like music or a door closing,'
+                'then describe it briefly in parentheses, like (door closing)'
+                'If there is no prominent sound in the clip, just type a space in the box.'
+                'Please watch the clip more than once if you need to, we want the most accurate subtitles!')
+        description_tag = ('<b>Watch a short clip and write down the dialogue between characters.<br/><br/><br/>' 
+                'If there is no dialogue, but there are prominent sounds like music or a door closing,'
+                'then describe it briefly in parentheses, like (door closing)<br/><br/><br/>'
+                'If there is no prominent sound in the clip, just type a space in the box.<br/><br/><br/>'
+                'Please watch the clip more than once if you need to, we want the most accurate subtitles!</b>')
+        keywords = 'video, transcript, translate'
+        salary = self.ui.payTextBox.toPlainText()
+        #---------------  BUILD OVERVIEW -------------------
+        overview = Overview()
+        overview.append_field('Title', 'Transcribe a short video clip')
+        #---------------  BUILD MOVIE CLIP -------------------
+        qc1 = QuestionContent() # I will modify this to embed some codes indicating time frame
+        #qc1.append_field('Text',description)
+        qc1.append_field('FormattedContent','<![CDATA[<br/>'+description_tag+'<br/><br/><br/><iframe width="600" height="400" src="'+ Url.replace('&','&amp;') + '">No video supported</iframe>]]>')
+        fta1 =  FreeTextAnswer()
+        q1 = Question(identifier='Answer',content=qc1,answer_spec=AnswerSpecification(fta1),is_required=True)
+        #looking for start time
+        #The end time can be computed by +10
+        
+        
+        start = Url.find('start=') +6
+        end = Url.find('&end',start)
+        time_frame = [('Time',Url[start:end])]
+        qc2 = QuestionContent()
+        qc2.append_field('Title',' ')
+        fta2 = SelectionAnswer(min=1,max=1, style='dropdown',selections=time_frame,type='text',other=False)
+        q2 = Question(identifier='Time code',content=qc2,answer_spec=AnswerSpecification(fta2),is_required=False)        
+        
+        #--------------- BUILD THE QUESTION FORM ------------------#
+        question_form = QuestionForm()
+        question_form.append(overview)
+        question_form.append(q1)
+        question_form.append(q2)
+        my_hit = mtc.create_hit(questions=question_form, lifetime=60*60*12, max_assignments=1, title=title, description=description_notag, keywords=keywords, duration = 10*60,reward=salary)
+        #--------------- ASSIGN HITID TO ARRAY OF TIMEFRAME --------#
+        my_hit_id = my_hit[0].HITTypeId
+        print "Hit type id 1: %s" % my_hit[0].HITTypeId
 
-    @staticmethod
-    def get_all_reviewable_hits(mtc):
-        page_size = 50
-        hits = mtc.get_reviewable_hits(page_size=page_size)
-        print "Total results to fetch %s " % hits.TotalNumResults
-        print "Request hits page %i" % 1
-        total_pages = float(hits.TotalNumResults)/page_size
-        int_total= int(total_pages)
-        if(total_pages-int_total>0):
-            total_pages = int_total+1
-        else:
-            total_pages = int_total
-        pn = 1
-        while pn < total_pages:
-            pn = pn + 1
-            print "Request hits page %i" % pn
-            temp_hits = mtc.get_reviewable_hits(page_size=page_size,page_number=pn)
-            hits.extend(temp_hits)
-        return hits    
+        #print self.hit_time_frame[my_hit_id]
+        print my_hit_id
+        #mtc.disable_hit(my_hit_id)
 
     def make_hit_decision(self,hit_results):
         #for hit in hit_results:
@@ -215,6 +246,12 @@ class MainWindow ( QMainWindow ):
             else:
                 mtc.reject_assignment(row[3])
                 print("Rejected >> %s" %row)
+    
+    def delete_all_hits(self):
+        hits2 = mtc.get_all_hits()
+        for hit in hits2:
+            mtc.disable_hit(hit.HITId)
+        self.ui.plainTextEdit.setPlainText('Deleted All Hits Successfully!!!!')
     
     @pyqtSlot()
     def splitFile(self):
